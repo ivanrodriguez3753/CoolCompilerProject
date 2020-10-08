dc4e71f0d869b591d2f295b01721633d91df6950
Ivan Rodriguez <ivanrodriguez@Ivans-MacBook-Pro.local>
2020-10-07 23:41:40 -0500

    setup test harness and add test resources

---
+++ tests/src/dummyTest.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "Tag.h"
+#include "lex.yy.h"
+
+
+using namespace std;
+
+TEST(newSuite, firstTest) {
+
+}
+
+
---
+++ tests/src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Ivan Rodriguez on 10/7/20.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
